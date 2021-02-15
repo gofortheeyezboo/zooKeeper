c1a9f4ad5372c271c95ec39de51fac65bb401d4c
Zac Grove <gofortheeyezboo@gmail.com>
2021-02-15 14:35:50 -0700

    commit

---
+++ app.js
@@ -0,0 +1,618 @@
+let zoo = []
+let warmBloods = []
+let coldBloods = []
+let mudBloods = []
+let hoofers = []
+let nonHoofers = []
+class Alpaca{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        lanolin = 'like sheeps wool'
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.lanolin = lanolin
+    }
+}
+class Bonobo{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        grip = ''
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.grip = grip;
+    }
+}
+class Cat{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        lives = 9        
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.lives = lives;
+    }
+}
+class Dingo{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        maybe = 'The Dingo ate your baby'        
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.maybe = maybe;
+    }
+}
+class Elephant{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        forgets = false        
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.forgets = forgets;
+    }
+}
+class Fox{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        say = ''  
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.say = say
+    }
+}
+class Giraffe{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = 'hooves',
+        neck = 'long'
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet
+        this.neck = neck
+    }
+}
+class Hippopotamus{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = 'webbed',
+        hungry = true
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet
+        this.hungry = hungry
+    }
+}
+class Iguana{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = 'claws',
+        tail = ''
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet
+        this.tail = tail
+    }
+}
+class Jaguar{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = 'paws',
+        stealthy = true
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet
+        this.stealthy = stealthy
+    }
+}
+class Kangaroo{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        pouch = '',
+        boxing = ''
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet
+        this.pouch = pouch
+        this.boxing = boxing      
+    }
+}
+class Lemur{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        hoppy = true
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.hoppy = hoppy;        
+    }
+}
+class Minotaur{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        habitat = 'labrynth'
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet
+        this.habitat = habitat         
+    }
+}
+class Ninetails{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        type = 'fire',
+        tails = 9
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.type = type;
+        this.tails = tails;
+    }
+}
+class Ostrich{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        flight = false,
+        scared = 'head in sand'
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.flight = flight;
+        this.scared = scared;       
+    }
+}
+class Pengolin{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        covid = 'positive'
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.covid = covid       
+    }
+}
+class Quetzotl{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        demon = true
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.demon = demon      
+    }
+}
+class Rhinoceros{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        plasty = true
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.plasty = plasty 
+    }
+}
+class Snek{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        sneaky = true
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.sneaky = sneaky;       
+    }
+}
+class Tortoise{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        shell = true
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.shell = shell
+    }
+}
+class Urchin{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        exterior = 'spiky',
+        street = true
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.exterior = exterior
+        this.street = street         
+    }
+}
+class Vole{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        earthdweller = true
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.earthdweller = earthdweller
+    }
+}
+class Walrus{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        tusks = true,
+        bone = 'thicc'
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.tusks = tusks
+        this.bone = bone     
+    }
+}
+class Xylophone{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        rarity = 'ultra',
+        animal = false
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.rarity = rarity;
+        this.animal = animal         
+    }
+}
+class Yak{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = 'hooves',
+        hair = 'long'
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.hair = hair           
+    }
+}
+class Zebra{
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        blood = '',
+        food = '',
+        feet = '',
+        horse = false,
+        stripy = true
+    ){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.blood = blood;
+        this.food = food;
+        this.feet = feet;
+        this.horse = horse;
+        this.stripy = stripy        
+    }
+}
+
+let al = new Alpaca('Al','Brown', 'Male', 100, 'warm', 'grass', )
+zoo.push(al)
+let barry = new Bonobo('Barry','Black', 'Male', 1000, 'warm', 'grass and nuts and stuff','strong af' )
+zoo.push(barry)
+let kitty = new Cat('Kitty','Orange', 'Female', 10, 'warm', 'mice', )
+zoo.push(kitty)
+let doggo = new Dingo('Doggo','Brown', 'Male', 40, 'warm', 'babies', )
+zoo.push(doggo)
+let oly = new Elephant('Olyphany','Grey', 'Male', 4000, 'warm', 'grass',false )
+zoo.push(oly)
+let fennick = new Fox('Fen','Orange', 'Male', 30, 'warm', 'chickens','rng rng rng rng' )
+zoo.push(fennick)
+let gerald = new Giraffe('Gerald','Yellow', 'Male', 1000, 'warm', 'acacia leaves','hooves','long as' )
+zoo.push(gerald)
+let harold = new Hippopotamus('Harold','Grey', 'Male', 5000, 'warm', 'leaves','webbed toesies',true )
+zoo.push(harold)
+let fred = new Iguana('Frederico','Green', 'Male', 5, 'cold', 'bugs maybe','claws','long as a muv' )
+zoo.push(fred)
+let chester = new Jaguar('Chester','Yellow', 'Male', 100, 'warm', 'antelope and cheetos','paws',true )
+zoo.push(chester)
+let roger = new Kangaroo('Roger','Brown', 'Male', 100, 'warm', 'grass and such','hoppers','no pouch','champ' )
+zoo.push(roger)
+let zabumafu  = new Lemur('Zabumafu','White', 'Male', 10, 'warm', 'grass, bugs, fruit', 'hoppers',true )
+zoo.push(zabumafu)
+let perseus = new Minotaur('Perseus','Brown', 'Male', 10000, 'warm', 'people','hooves','labrynth' )
+zoo.push(perseus)
+let ember = new Ninetails('Ember','White', 'Female', 200, 'warm', 'rare candy, water, lemonade','paws','fire',9 )
+zoo.push(ember)
+let ozzie = new Ostrich('Ozzie','Black n white', 'Male', 100, 'warm', 'bugs','big booters',false,'head in sand' )
+zoo.push(ozzie)
+let snacky = new Pengolin('Snacky','Brown', 'Male', 30, 'warm', 'grass','little diggers','positive' )
+zoo.push(snacky)
+let jake = new Quetzotl('Jake','Red', 'Male', 1000, 'yes plz', 'souls','demon claws',true )
+zoo.push(jake)
+let tim = new Rhinoceros('Time','Grey', 'Male', 3000, 'warm', 'grass','big ole honkers',true )
+zoo.push(tim)
+let solomon = new Snek('Solomon','Green', 'Male', 100, 'cold', 'anything that breathes and fits','no feets',true )
+zoo.push(solomon)
+let tommy = new Tortoise('Tommy','Brown', 'Male', 100, 'warm', 'grass','nubs',true )
+zoo.push(tommy)
+let lexi = new Urchin('Alexi','Purple?', 'Male', 5, 'warm', 'sea particles','no one really knows','spiky',true )
+zoo.push(lexi)
+let verlon = new Vole('Verlon','Brown', 'Male', 10, 'warm', 'grass','shovels',true )
+zoo.push(verlon)
+let willie = new Walrus('Wille','Brown', 'Male', 5000, 'warm', 'fishies', 'flippers', true, 'thicc')
+zoo.push(willie)
+let phony = new Xylophone('Phony','Brown', 'Male', 10, 'none', 'n/a','n/a','suuuper rare',false)
+zoo.push(phony)
+let yankovich = new Yak('Yankovich','Brown', 'Male', 2000, 'warm', 'grass','hooves','very long' )
+zoo.push(yankovich)
+let zed = new Zebra('Zed','Black/White', 'Male', 1000, 'warm', 'grass','hooves',false,true )
+zoo.push(zed)
+
+zoo.forEach(element => {
+    if(element.blood == 'warm'){
+    warmBloods.push(element)
+}else{
+    coldBloods.push(element)
+}
+    
+});
+
+mudBloods.push('Hermione')
